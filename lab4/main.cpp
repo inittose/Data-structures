b03291301755dcf98821a7bdf549b762e5810317
Konstantin Polushvayko <pol.konnik@gmail.com>
2023-11-12 16:55:48 +0700

    Удалил XOR демонстрацию

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -4,8 +4,6 @@
 
 using namespace std;
 
-void XORPreview();
-
 int main()
 {
     HashTable hashtable;
@@ -15,22 +13,3 @@ int main()
     return 0;
 }
 
-/*!
- * \brief Показывает работу бинарного оператора исключающего ИЛИ
- */
-void XORPreview()
-{
-    unsigned char a;
-    unsigned char b;
-    cout << "Enter a = ";
-    cin >> a;
-    cout << "Enter b = ";
-    cin >> b;
-    cout << "a = " << int(a) << "\t\tin bits: " << bitset<8>(a)
-         << "\nb = " << int(b) << "\t\tin bits: " << bitset<8>(b)
-         << "\na XOR b = " << (a ^ b) << "\tin bits: " << bitset<8>(a ^ b) << endl;
-    a = (a^b);
-    b = (a^b);
-    a = (a^b);
-    cout << "Swap a and b: \na = " << a << "\nb = " << b << endl;
-}
